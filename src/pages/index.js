3037c73638a99fb06f725e463e982833c4984c13
pabichw <pabichwiktor@gmail.com>
2020-11-04 13:51:30 +0100

    deploy trigger
    cms update
    
    cms update
    
    move cms
    
    ls before mv
    
    mv attempt 2
    
    pwd
    
    mv again ....
    
    routes fix
    
    routes fix 2
    
    about test
    
    redirect on 404

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <h1>Home page</h1>
+  return <><h1>Home page</h1>
+    <Link to="/about/">Go to About</Link></>
 }
